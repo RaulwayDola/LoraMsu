edc955d8241c62180fba88501f04df09caf9d9e1
RaulwayDola <139829817+RaulwayDola@users.noreply.github.com>
2023-07-22 03:53:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "27370260"))
 API_HASH = getenv("API_HASH", "3d414789f6fb615cc48341b12c624c22")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5662659781:AAEo-feRH2XoUq4glvCS8")
+BOT_TOKEN = getenv("BOT_TOKEN", "5662659781:AAEo-feRH2XoUq4glvCS8-HqdapjSsEeIpk")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://userbot:userbot@cluster0.x6kstu2.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001246079685"))
